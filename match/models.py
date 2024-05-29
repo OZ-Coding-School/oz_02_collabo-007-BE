f7b28033b6a970c2aec6ad662c5d91300721860e
hotbari <138793425+hotbari@users.noreply.github.com>
2024-05-29 10:39:50 +0900

    feat: Match 모델 생성

--- match/models.py
+++ match/models.py
@@ -2,14 +2,18 @@ from django.db import models
 from club.models import Club
 from users.models import CustomUser
 from core.models import TimeStampedModel, SoftDeleteModel
+from competition.models import Competition
+from participant_info.models import ParticipantInfo
 
-class Coach(TimeStampedModel, SoftDeleteModel):
-    id = models.AutoField(primary_key=True)
-    club = models.ForeignKey(Club, models.DO_NOTHING)
-    user = models.ForeignKey(CustomUser, models.DO_NOTHING)
-
-    def __int__(self):
-        return self.id
+class Match(models.Model):
+    id = models.IntegerField(primary_key=True)
+    matchround = models.IntegerField(db_column='matchRound', blank=True, null=True)  # Field name made lowercase.
+    matchnumber = models.IntegerField(db_column='matchNumber', blank=True, null=True)  # Field name made lowercase.
+    courtnumber = models.IntegerField(db_column='courtNumber', blank=True, null=True)  # Field name made lowercase.
+    competiton = models.ForeignKey(Competition, models.DO_NOTHING)
+    a_team = models.ForeignKey(ParticipantInfo, models.DO_NOTHING)
+    b_team = models.ForeignKey(ParticipantInfo, models.DO_NOTHING, related_name='match_b_team_set')
 
     class Meta:
-        db_table = 'coach'
+        managed = False
+        db_table = 'match'
