df2a27f76580f2225f5bee38726255f40b5b9488
hotbari <138793425+hotbari@users.noreply.github.com>
2024-06-01 23:58:18 +0900

    feat:Competition Apply Result View

--- applicant/serializers.py
+++ applicant/serializers.py
@@ -4,8 +4,21 @@ from users.models import CustomUser
 class ApplicantSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
 
-
-
     class Meta:
         model = Applicant
         fields = '__all__'
+
+
+class CompetitionApplicantSerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+    user_phone = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = ['user_name','user_phone']
+
+    def get_user_name(self, obj):
+        return obj.user.username  # Applicant의 user 모델의 name 필드 반환
+
+    def get_user_phone(self, obj):
+        return obj.user.phone  # Applicant의 user 모델의 phone 필드 반환
