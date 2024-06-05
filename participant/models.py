911db9574e4c01997931be4960e92d708eeede2f
hotbari <138793425+hotbari@users.noreply.github.com>
2024-06-06 03:08:01 +0900

    fix: 필드명 applicant_info -> participant_info

--- participant/models.py
+++ participant/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from core.models import TimeStampedModel, SoftDeleteModel
-from applicant_info.models import ApplicantInfo
+from participant_info.models import ParticipantInfo
 from users.models import CustomUser
 
 class Participant(TimeStampedModel, SoftDeleteModel):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    applicant_info = models.ForeignKey(ApplicantInfo, on_delete=models.CASCADE)
+    participant_info = models.ForeignKey(ParticipantInfo, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f"유저:{self.user.username} / 신청한 대회:{self.applicant_info.competition.name} / 신청정보:{self.applicant_info.id}"    
+        return f"유저:{self.user.username} / 신청한 대회:{self.participant_info.competition.name} / 신청정보:{self.participant_info.id}"    
     
     
     class Meta:
