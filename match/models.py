45a05a5e09f20b6bf8e66f88f38629dde9654b69
ingwanho <155050707+ingwanho@users.noreply.github.com>
2024-06-07 16:13:36 +0900

    refactor: 대회현황 매치부분 null값 추가(#73)

--- match/models.py
+++ match/models.py
@@ -9,10 +9,10 @@ class Match(TimeStampedModel):
     matchnumber = models.IntegerField(db_column='matchNumber', blank=True, null=True)  # Field name made lowercase.
     courtnumber = models.IntegerField(db_column='courtNumber', blank=True, null=True)  # Field name made lowercase.
     description = models.TextField(blank=True, null=True)  
-    winner_id = models.ForeignKey(ParticipantInfo, models.DO_NOTHING, related_name='winner_user')
+    winner_id = models.ForeignKey(ParticipantInfo, models.DO_NOTHING, related_name='winner_user', blank=True, null=True)
     competition = models.ForeignKey(Competition, models.DO_NOTHING)
-    a_team = models.ForeignKey(ParticipantInfo, models.DO_NOTHING, related_name='match_a_team_set')
-    b_team = models.ForeignKey(ParticipantInfo, models.DO_NOTHING, related_name='match_b_team_set')
+    a_team = models.ForeignKey(ParticipantInfo, models.DO_NOTHING, related_name='match_a_team_set', blank=True, null=True)
+    b_team = models.ForeignKey(ParticipantInfo, models.DO_NOTHING, related_name='match_b_team_set', blank=True, null=True)
 
     class Meta: 
         db_table = 'match'
