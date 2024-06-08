a900974a5a90defb521ebc29fcbbdb1826407aa2
ingwanho <155050707+ingwanho@users.noreply.github.com>
2024-06-08 20:13:48 +0900

    refactor: null값수정 대회현황(#81)

--- match/views.py
+++ match/views.py
@@ -11,16 +11,16 @@ from drf_yasg import openapi
 class MatchRoundAPIView(APIView):
     @swagger_auto_schema(
         manual_parameters=[
-            openapi.Parameter('round_number', openapi.IN_QUERY, description="Match round number", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('roundnumber', openapi.IN_QUERY, description="Match round number", type=openapi.TYPE_INTEGER),
         ],
         operation_summary='대회현황',
-        operation_description='쿼리파라미터를 사용해서 매치 라운드통해 정보를 불러올 수 있습니다.(예시 http://127.0.0.1:8000/api/v1/competitions/1/status/?round_number=1)')
+        operation_description='쿼리파라미터를 사용해서 매치 라운드통해 정보를 불러올 수 있습니다.(예시 http://127.0.0.1:8000/api/v1/competitions/1/status/?roundnumber=1)')
     #match_round와 competition_id를 통해 매치 정보 가져오기
     def get(self, request, competition_id):
-        round_number = request.query_params.get('round_number', None)
+        roundnumber = request.query_params.get('roundnumber', None)
         filters = {'competition_id': competition_id}
-        if round_number is not None:
-            filters['matchround'] = round_number
+        if roundnumber is not None:
+            filters['matchround'] = roundnumber
 
         matches = Match.objects.filter(**filters) if filters else Match.objects.all()
   
