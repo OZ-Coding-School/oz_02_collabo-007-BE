71493e4c0f188356499b96f880c56b22fa50251d
ingwanho <155050707+ingwanho@users.noreply.github.com>
2024-06-13 16:01:34 +0900

    feat: 내 전적 보기(#115)

--- match/urls.py
+++ match/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MatchRoundAPIView
+from .views import MatchRoundAPIView, MatchRecordAPIView
 
 urlpatterns = [
     path('competitions/<int:competition_id>/status/', MatchRoundAPIView.as_view(), name='competitions_status'),
+    path('competitions/record/', MatchRecordAPIView.as_view(), name='record'),
 ]
--- match/views.py
+++ match/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import MatchSerializer
+from .serializers import MatchSerializer, MatchRecordSerializer
 from .models import Match
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from users.models import CustomUser
 
 # 토너먼트리그 대회현황 조회
 class MatchRoundAPIView(APIView):
@@ -26,3 +27,25 @@ class MatchRoundAPIView(APIView):
   
         serializer = MatchSerializer(matches, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class MatchRecordAPIView(APIView):
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('usersid', openapi.IN_QUERY, description="User ID", type=openapi.TYPE_INTEGER),
+        ],
+        operation_summary='내 전적 조회',
+        operation_description='쿼리파라미터를 사용해서 유저의 매치 전적을 조회합니다.(예시 http://127.0.0.1:8000/api/v1/competitions/record/?user_id=1)'
+        )
+    def get(self, request):
+        user_id = request.query_params.get('user_id', None)
+        if user_id is None:
+            return Response("존재하지 않는 user_id입니다.", status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            user = CustomUser.objects.get(id=user_id)
+        except CustomUser.DoesNotExist:
+            return Response("유효하지 않은 user_id입니다.", status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MatchRecordSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
