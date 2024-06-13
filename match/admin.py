37a2f4cd98185bf6f21fbb4fd6c1d37986499c16
hotbari <138793425+hotbari@users.noreply.github.com>
2024-06-13 16:16:19 +0900

    chore: Django Admin custom

--- match/admin.py
+++ match/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Match
+from participant.models import Participant
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('id', 'competition', 'winner_id', 'a_team', 'b_team')
+    ordering = ('-created_at',)
 
-admin.site.register(Match)
-# Register your models here.
+
+admin.site.register(Match, MatchAdmin)
+
