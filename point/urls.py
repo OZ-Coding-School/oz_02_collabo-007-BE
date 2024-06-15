7a4e4d938f7be97d546d9aa8325432c90a088077
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-06-15 15:59:54 +0900

    hotfix : point url 수정

--- point/urls.py
+++ point/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import UserRankingView, TeamRankingView, RealtimeMyRankingView, RealtimeMyTeamRankingView
 
 urlpatterns = [
-    path('ranking/user', UserRankingView.as_view(), name='user_ranking'),
-    path('ranking/team', TeamRankingView.as_view(), name='team_ranking'),
-    path('ranking/realtime/myrank', RealtimeMyRankingView.as_view(), name='realtime_my_ranking'),
-    path('ranking/realtime/myteamrank', RealtimeMyTeamRankingView.as_view(), name='realtime_my_team_ranking'),
+    path('ranking/user/', UserRankingView.as_view(), name='user_ranking'),
+    path('ranking/team/', TeamRankingView.as_view(), name='team_ranking'),
+    path('ranking/realtime/myrank/', RealtimeMyRankingView.as_view(), name='realtime_my_ranking'),
+    path('ranking/realtime/myteamrank/', RealtimeMyTeamRankingView.as_view(), name='realtime_my_team_ranking'),
     
 ]
